d442d38d89948beb4ce1990f9209b56bae88d6c1
ryu08114 <87298247+ryu08114@users.noreply.github.com>
2021-07-13 09:47:29 +0900

    Add files via upload

--- cookie.js
+++ cookie.js
@@ -2,23 +2,30 @@ $(function() {
   $.cookie('test_cookie');
   var cookie = $.cookie('test_cookie');
 
-  $(window).on('load',function() {
-    if(cookie == 1) {
-      alert('cookieがあります');
-    } else {
-      $.magnificPopup.open({
-        items: {src: '#modal'},
-        type: 'inline', 
-        closeOnBgClick:true
-      })
-    }
-  })
+  if(cookie == 1) {
+    //cookieが保存されていたらアラートを表示
+    alert('cookieがあります');
+  } else {
+    //cookieが保存されていなかったらモーダルを表示
+    $.magnificPopup.open({
+      items: {src: '#modal'},
+      type: 'inline', 
+      closeOnBgClick:true
+    })
+  }
 
+  //追加ボタンでcookieを保存
   $('.add').on('click',function() {
     $.cookie('test_cookie',1,{expires: 183,});
   })
 
+  //削除ボタンでcookieを削除
   $('.remove').on('click',function() {
     $.removeCookie('test_cookie');
+  })
+
+  //閉じるボタンでモーダルを閉じる
+  $('.closeBtn').on('click',function() {
+    $('#modal').magnificPopup('close');
   })
 });
